beb12d0d23b7f47fd8dc52d6538a061de254a544
Jeremy Frank <jeremy@subspace.network>
2025-04-23 23:08:47 +0000

    add slack tool

--- src/agent.ts
+++ src/agent.ts
@@ -1,13 +1,14 @@
 import { createOrchestratorConfig } from './config/orchestrator.js';
 import { ConfigInstance } from './config/types.js';
-import { createTwitterTool } from './tools.js';
+import { createTwitterTool, createSlackTool } from './tools.js';
 
 export const createAgentRunnerOptions = async (configInstance: ConfigInstance) => {
   // Define custom tools
   const twitterAgent = await createTwitterTool(configInstance);
+  const slackAgent = await createSlackTool(configInstance);
 
   // Collect custom tools into an array to be passed to the orchestrator
-  const tools = [twitterAgent].filter(tool => tool !== undefined);
+  const tools = [twitterAgent, slackAgent].filter(tool => tool !== undefined);
 
   // Create orchestrator config
   const orchestratorConfig = await createOrchestratorConfig(configInstance, [...tools]);
