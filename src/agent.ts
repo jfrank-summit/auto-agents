4bd466addef51cabce962cb06de541f1668fbc26
Jeremy Frank <jfrank@summitinvest.tech>
2025-04-28 10:51:17 -0600

    integrate github agent with deployed agent code

--- src/agent.ts
+++ src/agent.ts
@@ -1,31 +1,24 @@
 import { createFirecrawlTools, createWebSearchTool } from '@autonomys/agent-core';
-
 import { createOrchestratorConfig } from './config/orchestrator.js';
 import { ConfigInstance } from './config/types.js';
-import { createSlackTool, createTwitterTool } from './tools.js';
-import { createGithubTools } from './tools/github-mcp/index.js';
+import { createSlackTool, createTwitterTool, createGithubTool } from './tools.js';
 
 export const createAgentRunnerOptions = async (configInstance: ConfigInstance) => {
   const { config } = configInstance;
-  // Define custom tools
+  // Define custom agents
   const twitterAgent = await createTwitterTool(configInstance);
   const slackAgent = await createSlackTool(configInstance);
+  const githubAgent = await createGithubTool(configInstance, ['all']);
+
+  // Define custom tools
   const firecrawlTools = config.FIRECRAWL_API_KEY
     ? await createFirecrawlTools(config.FIRECRAWL_API_KEY)
     : [];
   const webSearchTool = config.SERPAPI_API_KEY ? [createWebSearchTool(config.SERPAPI_API_KEY)] : [];
-  const githubTools = process.env.GITHUB_TOKEN
-    ? await createGithubTools(process.env.GITHUB_TOKEN)
-    : [];
 
-  // Collect custom tools into an array to be passed to the orchestrator
-  const tools = [
-    twitterAgent,
-    slackAgent,
-    ...webSearchTool,
-    ...githubTools,
-    ...firecrawlTools,
-  ].filter(tool => tool !== undefined);
+  const tools = [twitterAgent, slackAgent, githubAgent, ...webSearchTool, ...firecrawlTools].filter(
+    tool => tool !== undefined,
+  );
 
   // Create orchestrator config
   const orchestratorConfig = await createOrchestratorConfig(configInstance, [...tools]);
